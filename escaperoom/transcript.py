438cb0f10755a987cfb8380608d0b9f105bf42a9
Raphael Schmitz <schmitz-raphael@proton.me>
2026-09-26 10:26:28 +0200

    fixed transcript

--- escaperoom/transcript.py
+++ escaperoom/transcript.py
@@ -1,7 +1,7 @@
 
-class Transcript:
+class Transcriptor:
     def __init__(self, filename):
-        self.file = open(filename, "rw")
+        self.file = open(filename, "w")
 
     def write_line(self, line):
         self.file.writelines(line)
