8d2ab87404fd406e8c21831efd18655deedd13d7
Raphael Schmitz <schmitz-raphael@proton.me>
2026-09-26 10:26:13 +0200

    Main File calling eval loop of engine

--- escape.py
+++ escape.py
@@ -1,8 +1,15 @@
 import sys
 
+from escaperoom.Engine.engine import Engine
+
+
 
 def main(argv):
     print("Hello world")
+    engine = Engine("run.txt")
+    engine.play()
+
+
 
 if __name__ == "__main__":
     main(sys.argv)
